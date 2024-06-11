d8592e648790be35a70c2a03521119848efeed4d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-06-11 01:04:13 -0700

    chore: Update GitHub Actions workflow to upload renv.lock to GitHub Artifacts

--- install_dep.r
+++ install_dep.r
@@ -2,5 +2,16 @@ options(unzip = Sys.which("unzip"))
 Sys.which("tar")
 install.packages('remotes')
 install.packages('devtools')
+install.packages('renv')
+
+# Initialize renv and restore from the lockfile if it exists
+if (!file.exists("renv.lock")) {
+  renv::init(bare = TRUE)
+} else {
+  renv::restore()
+}
 remotes::install_github('mitchelloharawild/icons@v0.1.0')
 devtools::install_github("nstrayer/datadrivencv")
+
+# Snapshot the current state of the library
+renv::snapshot()
