ac2079f3f8f55345f518c748db23e8e9058d7a4a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-06-10 23:59:26 -0700

    chore: Update install_dep.r to install 'nstrayer/datadrivencv' from the 'master' branch

--- install_dep.r
+++ install_dep.r
@@ -1,4 +1,4 @@
 options(unzip = Sys.which("unzip"))
 Sys.which("tar")
 Sys.setenv(GITHUB_PAT = Sys.getenv("GITHUB_TOKEN"))
-devtools::install_github("nstrayer/datadrivencv")
+devtools::install_github("nstrayer/datadrivencv", ref = "master")
