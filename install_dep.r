6bc1fdd192d7e4954c7e342a6077fee16f3014ff
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-06-11 01:46:13 -0700

    chore: Update r-base dependency to version 4.1.2

--- install_dep.r
+++ install_dep.r
@@ -1,14 +1,14 @@
 options(unzip = Sys.which("unzip"))
 Sys.which("tar")
 
-install.packages('renv')
+# install.packages('renv')
 
-# Initialize renv and restore from the lockfile if it exists
-if (!file.exists("renv.lock")) {
-  renv::init(bare = TRUE)
-} else {
-  renv::restore()
-}
+# # Initialize renv and restore from the lockfile if it exists
+# if (!file.exists("renv.lock")) {
+#   renv::init(bare = TRUE)
+# } else {
+#   renv::restore()
+# }
 install.packages(c('remotes', 'devtools'))
 remotes::install_github('mitchelloharawild/icons@v0.1.0')
 devtools::install_github("nstrayer/datadrivencv")
