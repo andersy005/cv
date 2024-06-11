dc365690ef2acdc181e91e161cd0d9d1a9ca1963
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-06-11 00:46:11 -0700

    chore: Update install_dep.r to install 'nstrayer/datadrivencv' from the 'master' branch

--- install_dep.r
+++ install_dep.r
@@ -2,4 +2,7 @@ options(unzip = Sys.which("unzip"))
 Sys.which("tar")
 install.packages('remotes')
 install.packages('devtools')
+if (!requireNamespace("icon", quietly = TRUE)) {
+  remotes::install_github("rstudio/icon")
+}
 devtools::install_github("nstrayer/datadrivencv")
