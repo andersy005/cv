ee17251f1b134c20441c0e27a5bb36d853261b13
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-06-11 00:28:56 -0700

    chore: Update GitHub Actions workflow to use 'main' branch instead of 'master'

--- install_dep.r
+++ install_dep.r
@@ -1,5 +1,4 @@
 options(unzip = Sys.which("unzip"))
 Sys.which("tar")
-Sys.setenv(GITHUB_PAT = Sys.getenv("GITHUB_TOKEN"))
-devtools::install_github("mitchelloharawild/icons", ref = "master")
+# devtools::install_github("mitchelloharawild/icons", ref = "master")
 devtools::install_github("nstrayer/datadrivencv", ref = "master")
